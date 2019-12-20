c1cba438f64e655f76f63d76ff3043b8bb49859a
TonyWhiteb <tonybai2016@gmail.com>
2019-12-20 02:01:31 -0600

    Main_pylint_complete

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # pylint: disable= multiple-imports
+# pylint: disable= C0103
 import sys, os
 import wx
+
 from Frame import AppFrame
+
 
 
 if __name__ == '__main__':
@@ -9,8 +12,8 @@ if __name__ == '__main__':
     THISPYFILE = args.pop(0)
     argc = len(args)
     path = os.path.dirname(os.path.abspath(__file__))
-    app = wx.App(redirect= False)
-    appFrame = AppFrame.AppFrame(args, argc, file_path= path)
-    import wx.lib.inspection
+    app = wx.App(redirect=False)
+    appFrame = AppFrame.AppFrame(args, argc, file_path=path)
+    # import wx.lib.inspection
     app.MainLoop()
  
